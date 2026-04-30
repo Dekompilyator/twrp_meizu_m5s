079b2960f694fc4a4dcbc095d619d6d70ad08c1d
Dekompliyator <bzigd2023nohack@gmail.com>
2026-04-30 22:07:33 +0300

    Add data decrypt support for LineageOS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,6 +59,15 @@ TW_EXTERNAL_STORAGE_PATH := "/extSdCard"
 TW_INCLUDE_NTFS_3G := true
 
 
+# Encryption
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_CRYPTO_FBE := false
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/mtk-msdc.0/11230000.msdc0/by-name/userdata"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_METADATA_BLKDEV := "/dev/block/platform/mtk-msdc.0/11230000.msdc0/by-name/metadata"
+
+
 # TWRP
 TW_THEME := portrait_hdpi
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
@@ -66,13 +75,9 @@ TW_EXTRA_LANGUAGES := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 120
-TW_INCLUDE_CRYPTO := true
 
 
 # Battery and CPU
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/battery
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
-
-#Customization
-TW_DEVICE_VERSION := M612H-1.0.1 by @Dekompilyator
